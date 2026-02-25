bd83d9dd393efe5bec8e7547e5c2744513c5d1f9
morabii177 <morabii177@gmail.com>
2026-02-25 14:13:43 -0500

    Add files via upload

---
+++ colour garden/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'colour-garden-v1';
+const ASSETS = [
+  './',
+  './colour-garden.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install: cache all core files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate: clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Fetch: serve from cache first, fall back to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached => cached || fetch(event.request))
+  );
+});
